259fc67ca4ad1f17be3e639fd35c86cde408dec0
Alexander <alexgomezde@outlook.com>
2021-02-23 01:03:06 -0600

    Add project files.

---
+++ SmartPayment/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using SmartPayment.Models;
+
+namespace SmartPayment.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index(string message = "")
+        {
+            ViewBag.Message = message;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            SMART_PAYMENT_DBEntities db = new SMART_PAYMENT_DBEntities();
+
+
+            var cliente = db.CLIENTEs.FirstOrDefault(e => e.CLI_CORREO_ELECTRONICO == email && e.CLI_CONTRASENNA == password);
+
+            if (cliente != null)
+            {
+                FormsAuthentication.SetAuthCookie(cliente.CLI_CORREO_ELECTRONICO, true);
+                return RedirectToAction("Index", "Profile");
+            }
+            else
+            {
+                return RedirectToAction("Index", new { message = "Correo electrónico y/o contraseña" });
+            }
+ 
+        }
+
+        [Authorize]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+    }
+}
---
+++ SmartPayment/Controllers/ProfileController.cs
@@ -0,0 +1,44 @@
+﻿using SmartPayment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartPayment.Controllers
+{
+    public class ProfileController : Controller
+    {
+
+        [Authorize]
+        // GET: Profile
+        public ActionResult Index()
+        {
+            GetUsername();
+            return View();
+        }
+
+        public ActionResult GetUsername()
+        {
+            SMART_PAYMENT_DBEntities db = new SMART_PAYMENT_DBEntities();
+
+            var client = db.CLIENTEs.FirstOrDefault(x => x.CLI_CORREO_ELECTRONICO == System.Web.HttpContext.Current.User.Identity.Name);
+
+
+            string dob = client.CLI_FECHA_NACIMIENTO.ToString("yyyy-MM-dd");
+
+
+            ViewData["id"] = client.CLI_IDENTIFICACION;
+            ViewData["dateOfBirth"] = dob;
+            ViewData["name"] = client.CLI_NOMBRE;
+            ViewData["lastName"] = client.CLI_PRIMER_APELLIDO;
+            ViewData["secondLastName"] = client.CLI_SEGUNDO_APELLIDO;
+            ViewData["paymentMethod"] = client.CLI_METODO_PAGO;
+            ViewData["email"] = client.CLI_CORREO_ELECTRONICO;
+            ViewData["password"] = client.CLI_CONTRASENNA;
+            ViewData["password2"] = client.CLI_CONTRASENNA;
+            ViewData["monedero"] = client.CLI_MONEDERO;
+            return View();
+        }
+    }
+}
